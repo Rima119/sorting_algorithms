e99b01a52a53ed740aac3237544ff8b5caae4470
Rim El Filali <rimo1902@gmail.com>
2022-10-10 09:06:26 -0700

    solution to task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,6 +8,7 @@
  */
 void swap_node(listint_t **head, listint_t **node1, listint_t *node2)
 {
+	(*node1)->next = node2->next;
 	if (((*node1)->prev) != NULL)
 		(*node1)->prev->next = node2;
 	if ((node2->next) != NULL)
@@ -15,9 +16,9 @@ void swap_node(listint_t **head, listint_t **node1, listint_t *node2)
 	else
 		*head = node2;
 	(*node1)->prev = node2;
-	(*node1)->next = node2->next;
 	node2->prev = (*node1)->prev;
 	node2->next = *node1;
+	*node1 = node2->prev;
 }
 
 /**
