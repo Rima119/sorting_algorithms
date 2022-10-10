8f45b2171f1f07a5dc2105b038cf34cbe208424c
Rim El Filali <rimo1902@gmail.com>
2022-10-10 09:01:18 -0700

    solution to task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,18 +6,18 @@
  * @node1: a pointer to the first node of the list
  * @node2: a pointer to the second node of the list
  */
-void swap_node(listint_t **head, listint_t *node1, listint_t *node2)
+void swap_node(listint_t **head, listint_t **node1, listint_t *node2)
 {
-	if ((node1->prev) != NULL)
-		node1->prev->next = node2;
+	if (((*node1)->prev) != NULL)
+		(*node1)->prev->next = node2;
 	if ((node2->next) != NULL)
-		node2->next->prev = node1;
+		node2->next->prev = *node1;
 	else
 		*head = node2;
-	node1->prev = node2;
-	node1->next = node2->next;
-	node2->prev = node1->prev;
-	node2->next = node1;
+	(*node1)->prev = node2;
+	(*node1)->next = node2->next;
+	node2->prev = (*node1)->prev;
+	node2->next = *node1;
 }
 
 /**
