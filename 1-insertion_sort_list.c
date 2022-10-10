561840fd00d79c513c837d44ceb7f07a4688bc2d
Rim El Filali <rimo1902@gmail.com>
2022-10-10 08:21:21 -0700

    solution to task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - function that sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: doubly linked list of integers
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr = *list, *node, *swap;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	while ((curr = curr->next))
+	{
+		swap = curr;
+		while (swap->prev && swap->n < swap->prev->n)
+		{
+			node = swap->prev;
+			if (node->prev)
+				node->prev->next = swap;
+			else if (swap->next)
+				swap->next->prev = node;
+			else
+				*list = swap;
+			node->next = swap->next;
+			swap->prev = node->prev;
+			swap->next = node;
+			node->prev = swap;
+			print_list(*list);
+		}
+	}
+}
