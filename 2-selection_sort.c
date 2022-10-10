0873d5adaebd1be6af64a9d7571725a98eef2886
Rim El Filali <rimo1902@gmail.com>
2022-10-10 10:02:30 -0700

    solution to task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * swap_node - function that swaps two nodes
+ * @node1: a pointer to the first node of the list
+ * @node2: a pointer to the second node of the list
+ */
+void swap(int *node1, int *node2)
+{
+	int a = *node1;
+	*node1 = *node2;
+	*node2 = a;
+}
+
+/**
+ * selection_sort - function that sorts an array of integers
+ * in ascending order using the Selection sort algorithm
+ * @array: the sorted array
+ * @size: size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	unsigned int a, b, c;
+
+	if (array == NULL || size <= 0)
+		return;
+
+	for (a = 0; a < size - 1; a++)
+	{
+		b = a;
+		for (c = a + 1; c < size; c++)
+		{
+			if (array[c] < array[b])
+				b = c;
+		}
+		if (c != a)
+		{
+			swap(&array[c], &array[a]);
+			print_array(array, size);
+		}
+	}
+}
