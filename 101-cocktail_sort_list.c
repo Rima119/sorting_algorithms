cb4797e1864d8b7f4d59bc3b58abeb08f52f8600
Rim El Filali <rimo1902@gmail.com>
2022-10-11 15:55:15 -0700

    solution to task 5

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -6,21 +6,18 @@
  * @node1: a pointer to the first node of the list
  * @node2: a pointer to the second node of the list
  */
-void swap_node(listint_t *node1, listint_t *node2, listint_t **head)
+void swap_node(listint_t **head, listint_t *node1, listint_t *node2)
 {
-	listint_t *n1 = node1->prev;
-	listint_t *n2 = node2->next;
-
-	if (n1 != NULL)
-		n1->next = node2;
+	if ((node1->prev) != NULL)
+		node1->prev->next = node2;
 	else
 		*head = node2;
 	node1->prev = node2;
-	node1->next = n2;
-	node2->prev = n1;
+	node1->next = node2->next;
+	node2->prev = node1->prev;
 	node2->next = node1;
-	if (n2 != NULL)
-		n2->prev = node1;
+	if ((node2->next) != NULL)
+		node2->next->prev = node1;
 }
 
 /**
@@ -31,7 +28,7 @@ void swap_node(listint_t *node1, listint_t *node2, listint_t **head)
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *p;
-	int swapped = 0;
+	int swapped;
 
 	if (!list || !*list || !(*list)->next)
 		return;
@@ -41,20 +38,20 @@ void cocktail_sort_list(listint_t **list)
 		{
 			if (p->n > p->next->n)
 			{
-				swap_node(p, p->next, list);
+				swap_node(list, p, p->next);
 				print_list(*list);
 				swapped = 1;
 				p = p->prev;
 			}
 		}
-		if (swapped == 0)
+		if (swapped)
 			break;
 		swapped = 0;
 		for (p = *list; p->prev != NULL; p = p->prev)
 		{
 			if (p->n < p->prev->n)
 			{
-				swap_node(p->prev, p, list);
+				swap_node(list, p->prev, p);
 				swapped = 1;
 				print_list(*list);
 				p = p->next;
